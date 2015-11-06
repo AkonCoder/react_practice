9a02ca07b4b3399615586b969501b1c293f25e52
袁康 <yuankang@meituan.com>
2015-11-06 12:01:25 +0800

    update

--- react-started/app.js
+++ react-started/app.js
@@ -5,6 +5,27 @@ var ReactDom = require('react-dom');
 import React from 'react';
 import ColorPanel from './panel/colorPanel';
 
+//var Team = React.createClass({
+//    onHungry: function(name) {
+//        console.log(name + " is hungry");
+//    },
+//    render: function() {
+//        return <div>Team onwer is: <People name={this.props.name} onHungry={this.onHungry}></People></div>;
+//    }
+//});
+//var People = React.createClass({
+//    render: function() {
+//        return <span onClick={this.handleClick}>{this.props.name}</span>;
+//    },
+//    handleClick: function() {
+//        this.props.onHungry(this.props.name);
+//    }
+//});
+
+
+
 window.onload = function () {
     ReactDom.render(<ColorPanel />, document.getElementById('demos'));
+    //ReactDom.render(<Team name='Lucy' />,
+    //    document.getElementById('test'))
 }
